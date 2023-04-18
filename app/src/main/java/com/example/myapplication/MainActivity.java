def84788233d7d0edcbe9d01ab2502afc4caa9a1
DingHeng <jayleedingheng0401@gmail.com>
2023-04-18 19:36:21 +0800

    feat: Record Nutrients

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.BaseAdapter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
